51c6016d2e6930706f3594ec37e76954d99c3069
Occhino <vincentocchino@yahoo.com>
2025-08-02 21:12:26 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showImage () {
+    var image = document.getElementById("bsimage");
+    
+    if (image.style.display === "none") {
+        image.style.display = "block";
+    } else {
+        image.style.display = "none";
+    }   
+}
